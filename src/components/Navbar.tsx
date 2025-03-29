0380f54a2165d4e8840491083c064ab212a817ab
Izzsamara <izzidinsamara@gmail.com>
2025-03-29 21:38:37 +0300

    Add styling to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 export default function Navbar () {
     return (
         <>
-        <nav>
-            <div>
-                <h1>TechNova</h1>
-                <ul className="">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Contact</a></li>
+        <nav className="w-full">
+            <div className="max-w-6xl flex justify-between mx-auto h-16 items-center mt-4">
+                <h1 className="text-5xl font-bold">Tech<span className="text-cyan-700">Nova</span></h1>
+                <ul className="flex space-x-8 items-center">
+                    <li><a className="text-2xl hover:text-cyan-700 " href="#">Home</a></li>
+                    <li><a className="text-2xl hover:text-cyan-700"  href="#">About</a></li>
+                    <li><a className="text-2xl hover:text-cyan-700" href="#">Blog</a></li>
+                    <li><a className="text-2xl hover:text-cyan-700" href="#">Contact</a></li>
 
                 </ul>
 
