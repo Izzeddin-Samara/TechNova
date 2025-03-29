c72c06b7df8963968f0ad97abc13a53fbbb493cb
Izzsamara <izzidinsamara@gmail.com>
2025-03-29 18:12:09 +0300

    Add components directory with Navbar component structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+export default function Navbar () {
+    return (
+        <>
+        <nav>
+            <div>
+                <h1>TechNova</h1>
+                <ul className="">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+
+                </ul>
+
+            </div>
+            
+        </nav>
+        </>
+    );
+
+}
