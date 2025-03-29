9428730740aa93b604c9d8df7c83e161a902c704
Izzsamara <izzidinsamara@gmail.com>
2025-03-30 00:35:39 +0300

    Add Hero Section component

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,18 @@
+
+export default function HeroSection() {
+  return (
+    <>
+      <div className="flex items-center h-[650px]">
+        <div className="text-center w-full">
+          <h1 className="text-5xl w-4/6 mx-auto font-bold sm:text-6xl lg:text-7xl">
+            Empowering Your Business with Cutting-Edge Technology Solutions
+          </h1>
+          <p className="text-xl sm:text-2xl lg:text-2xl text-gray-600 mt-16 w-3/4 mx-auto">
+            At TechNova, we deliver innovative cloud, software, and security
+            solutions to drive your business forward.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
