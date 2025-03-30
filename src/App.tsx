3db28a1880d7f230fc4257206cfeef820ed568b5
Izzsamara <izzidinsamara@gmail.com>
2025-03-30 08:10:34 +0300

    Import and use Services component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import Services from './components/Services'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar/>
     <HeroSection/>
+    <Services/>
 
     </>
   )
