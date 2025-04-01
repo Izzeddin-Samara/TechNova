245cc473937d0db06d8a4f46b4d61e95fbc022ed
Izzsamara <izzidinsamara@gmail.com>
2025-04-01 10:46:55 +0300

    Add styling to why choose us component

--- src/components/Whychooseus.tsx
+++ src/components/Whychooseus.tsx
@@ -30,20 +30,20 @@ export default function Whychooseus() {
   ];
   return (
     <>
-      <div>
-        <div>
-          <h1>Why Choose us</h1>
+      <div className="w-full mt-8">
+        <div className="flex justify-center">
+          <h1 className="text-5xl font-bold mb-8 mr-4">Why Choose us</h1>
           <FaHandshake size={50} />
         </div>
 
-        <div></div>
-        <div>
-          <div>
+        <div className="w-2/4 h-2 bg-cyan-700 mx-auto mb-20"></div>
+        <div className="max-w-7xl mx-auto text-center">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 ml-8">
             {points.map((point) => (
-                <div>
-                <h1>{point.status}</h1>
-                <h3>{point.title}</h3>
-                <p>
+                <div className="text-center mx-auto">
+                <h1 className="text-6xl font-semibold text-cyan-700">{point.status}</h1>
+                <h3 className="text-md font-bold">{point.title}</h3>
+                <p className="text-gray-700 mt-4 text-justify text-sm">
                   {point.description}
                 </p>
               </div>
