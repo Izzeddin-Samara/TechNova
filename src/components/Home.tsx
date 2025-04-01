12f153d03c2f0f662783f9e807ecee7be4bfce87
Izzsamara <izzidinsamara@gmail.com>
2025-04-01 19:44:56 +0300

    Add Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,18 @@
+import HeroSection from "./HeroSection";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
+import Pricing from "./Pricing";
+import CTA from "./CTA";
+export default function Home() {
+  return (
+    <>
+    <HeroSection/>
+    <Services/>
+    <Testimonials/>
+    <Pricing/>
+    <CTA/>
+    </>
+
+  );
+  
+}
