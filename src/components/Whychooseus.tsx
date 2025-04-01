2a00f038db49e010d5061f2b4f2e8fa8af8f15a4
Izzsamara <izzidinsamara@gmail.com>
2025-04-01 10:25:11 +0300

    Add why choose us component with points array

---
+++ src/components/Whychooseus.tsx
@@ -0,0 +1,35 @@
+
+
+export default function Whychooseus() {
+  const points = [
+    {
+      status: "500+",
+      title: "Projects Completed",
+      description:
+        "We have successfully completed over 500 projects across various industries, ensuring quality and satisfaction",
+    },
+    {
+      status: "98%",
+      title: "Client Satisfaction",
+      description:
+        " Our clients consistently rate us highly, demonstrating our commitment to excellent service.",
+    },
+    {
+      status: "+10",
+      title: "Years of Industry Experience",
+      description:
+        "With over a decade of experience, we understand the intricacies of delivering top-notch solutions.",
+      
+    },
+    {
+      status: "100+",
+      title: "Happy Clients",
+      description:
+        "We have built long-term relationships with over 100+ satisfied clients who trust us with their projects.",
+    },
+  ];
+  return (
+    <>
+    </>
+  );
+}
