abf631b068322407505bb27b250839950b87d794
Izzsamara <izzidinsamara@gmail.com>
2025-04-03 19:09:05 +0300

    Add MissionVision component

---
+++ src/components/about/MissionVision.tsx
@@ -0,0 +1,35 @@
+export default function MissionVision() {
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <div>
+              <h1>
+                Mission
+              </h1>
+              <p>
+                At TechNova, our mission is to empower businesses through
+                innovative, secure, and scalable technology solutions. We strive
+                to help organizations achieve growth, efficiency, and security
+                by providing cutting-edge software development, cloud solutions,
+                and digital transformation services.
+              </p>
+            </div>
+            <div>
+              <h1>
+                Vision
+              </h1>
+              <p>
+                Our vision is to be the leading provider of transformative
+                technology solutions that revolutionize businesses, fostering a
+                world where innovation, security, and digital agility empower
+                organizations to thrive in an ever-evolving marketplace.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
