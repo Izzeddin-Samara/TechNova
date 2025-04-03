11ebf9e3f2080b9a62af97eae1488afde3024030
Izzsamara <izzidinsamara@gmail.com>
2025-04-03 19:46:46 +0300

    Add styling to MissionVision component

--- src/components/about/MissionVision.tsx
+++ src/components/about/MissionVision.tsx
@@ -1,14 +1,14 @@
 export default function MissionVision() {
   return (
     <>
-      <div>
-        <div>
-          <div>
+      <div className="w-full">
+        <div className="max-w-5xl mx-auto">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-20">
             <div>
-              <h1>
+              <h1 className="text-3xl text-center mt-8 font-bold text-cyan-700">
                 Mission
               </h1>
-              <p>
+              <p className="text-lg text-justify mt-8">
                 At TechNova, our mission is to empower businesses through
                 innovative, secure, and scalable technology solutions. We strive
                 to help organizations achieve growth, efficiency, and security
@@ -17,10 +17,10 @@ export default function MissionVision() {
               </p>
             </div>
             <div>
-              <h1>
+              <h1 className="text-3xl text-center mt-8 font-bold text-cyan-700">
                 Vision
               </h1>
-              <p>
+              <p className="text-lg text-justify mt-8">
                 Our vision is to be the leading provider of transformative
                 technology solutions that revolutionize businesses, fostering a
                 world where innovation, security, and digital agility empower
