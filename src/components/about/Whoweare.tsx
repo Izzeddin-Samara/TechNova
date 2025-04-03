06ae66a11668404c64bb8b3bb9916b6b156bd9ee
Izzsamara <izzidinsamara@gmail.com>
2025-04-03 18:55:01 +0300

    Add 'who we are' component in About directory

---
+++ src/components/about/Whoweare.tsx
@@ -0,0 +1,56 @@
+import { FaCircleInfo } from "react-icons/fa6";
+
+export default function Whoweare() {
+  return (
+    <>
+      <div>
+        <div>
+          <div className="">
+            <div>
+              <FaCircleInfo size={50} />
+              <h1>
+                Who we are?
+              </h1>
+            </div>
+
+            <p>
+              At TechNova, we are a dedicated team of technology experts with a
+              deep understanding of software development, cybersecurity, cloud
+              solutions, and digital transformation. With a passion for
+              innovation, we work tirelessly to provide businesses with the
+              tools they need to succeed in the ever-changing digital landscape.
+              Our team brings a wealth of experience in developing custom
+              software solutions, designing intuitive and engaging user
+              interfaces, and implementing robust cybersecurity measures that
+              protect businesses from digital threats. From cloud migration to
+              IT consulting, we help organizations of all sizes optimize their
+              technology infrastructure for better performance and growth.
+            </p>
+          </div>
+          <div>
+            <div>
+              <img src="office.webp" alt="office-img" />
+            </div>
+            <div>
+              <p>
+                We pride ourselves on being solution-oriented, always focused on
+                delivering secure, scalable, and high-performance systems that
+                meet the unique needs of each client. Whether it’s creating web
+                and mobile applications, enhancing user experiences, or guiding
+                businesses through their digital transformation journey, we are
+                committed to offering innovative and reliable solutions. Our
+                team thrives on collaboration, working closely with clients to
+                understand their challenges and deliver tailored solutions that
+                make a real impact. We value transparency, integrity, and
+                excellence, ensuring that every project we undertake is executed
+                to the highest standards. At TechNova, we don’t just build
+                software—we build long-lasting partnerships that help businesses
+                grow, adapt, and thrive in an increasingly digital world.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
