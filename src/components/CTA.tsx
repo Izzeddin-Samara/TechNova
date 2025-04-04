e4bcf4a64a460347ee0d2f7e19b085e0f5e979e3
Izzsamara <izzidinsamara@gmail.com>
2025-04-04 22:36:31 +0300

    Update CTA button to make it responsive for small screens

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -10,7 +10,7 @@ export default function CTA() {
           Let’s discuss how TechNova can help you innovate and grow.
         </p>
         <Link to="/contact">
-          <button className="bg-cyan-700 font-bold hover:bg-cyan-800 p-4 w-1/5 mt-8  text-2xl text-white rounded-lg focus:ring-4 focus:ring-cyan-500 cursor-pointer">
+          <button className="bg-cyan-700 font-bold hover:bg-cyan-800 p-4 w-3/5 md:w-1/5 mt-8  text-2xl text-white rounded-lg focus:ring-4 focus:ring-cyan-500 cursor-pointer">
             Contact Us now!
           </button>
         </Link>
