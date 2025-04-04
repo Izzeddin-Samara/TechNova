bef4955372754fd865b3f82599bc712f5c5fe890
Izzsamara <izzidinsamara@gmail.com>
2025-04-04 12:12:05 +0300

    Add styling to ApplicationForm component

--- src/components/careers/ApplicationForm.tsx
+++ src/components/careers/ApplicationForm.tsx
@@ -4,53 +4,57 @@ import { FaWpforms } from "react-icons/fa6";
 export default function ApplicationForm() {
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <h1>
+      <div className="w-full mt-60">
+        <div className="max-w-6xl mx-auto">
+          <div className="mb-20 text-center">
+            <h1 className="text-center text-4xl text-cyan-700 font-bold mb-10">
               Application Form
             </h1>
-            <p>
+            <p className="text-xl">
               We're excited that you're interested in joining our team!
             </p>
-            <p>
+            <p className="text-xl">
               Please fill out the application form below, and we’ll be in touch
               soon
             </p>
           </div>
-          <div>
-            <div>
-              <img src="join.png" alt="" />
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+            <div className="flex items-center justify-center">
+              <img  src="join.png" alt="application-img" />
             </div>
             <div>
-              <form>
+              <form className="space-y-8">
                 <div>
-                  <label>Full Name</label>
+                  <label className="text-xl">Full Name</label>
                   <input
+                    className="w-full p-3 bg-gray-100 outline-none border border-gray-300 rounded-lg mt-2 focus:ring-3 focus:ring-cyan-600"
                     type="text"
                   />
                 </div>
                 <div>
-                  <label>Email Address</label>
+                  <label className="text-xl">Email Address</label>
                   <input
+                    className="w-full p-3 bg-gray-100 outline-none border border-gray-300 rounded-lg mt-2 focus:ring-3 focus:ring-cyan-600"
                     type="email"
                     name=""
                     id=""
                   />
                 </div>
                 <div>
-                  <label>Mobile Number</label>
+                  <label className="text-xl">Mobile Number</label>
                   <input
+                    className="w-full p-3 bg-gray-100 outline-none border border-gray-300 rounded-lg mt-2 focus:ring-3 focus:ring-cyan-600"
                     type="number"
                   />
                 </div>
                 <div>
-                  <label>CV</label>
+                  <label className="text-xl">CV</label>
                   <input
+                    className="w-full p-3 bg-gray-100 outline-none border border-gray-300 rounded-lg mt-2 focus:ring-3 focus:ring-cyan-600"
                     type="file"
                   />
                 </div>
-                <button >
+                <button className=" w-full bg-cyan-700 hover:bg-cyan-800 text-white p-4 rounded-lg font-bold focus:ring-4 focus:ring-cyan-500 cursor-pointer text-xl">
                   Apply
                 </button>
               </form>
