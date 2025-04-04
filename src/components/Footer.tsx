828d65aa5e5839890127c150c88ba6b1ce115343
Izzsamara <izzidinsamara@gmail.com>
2025-04-04 22:25:40 +0300

    Update Footer style to ensure it's responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
   return (
     <>
       <footer className="mt-50 bg-gray-200 p-8">
-        <div className="max-w-8xl mx-auto">
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5  space-x-4">
+        <div className="max-w-8xl mx-auto text-center">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 space-y-8 space-x-4">
             <div>
               <h1 className="text-3xl font-bold">
                 Tech<span className="text-cyan-700">Nova</span>
               </h1>
-              <p className="w-3/4 text-md text-justify mt-5 mb-4">
+              <p className="w-3/4 text-md text-justify mt-5 mb-4 mx-auto">
                 At Technova, we are committed to empowering your business by
                 delivering cutting-edge, future-ready technology solutions that
                 are secure, scalable, and tailored to meet your unique goals.
@@ -158,20 +158,20 @@ export default function Footer() {
             <div>
               <h1 className="font-bold text-xl ">Connect with Us</h1>
               <ul className="space-y-4 mt-6">
-                <li className="flex items-center space-x-1">
+                <li className="flex items-center justify-center space-x-1">
                   <FaLocationDot />
                   <span>123 TechNova Street, City, Country</span>
                 </li>
-                <li className="flex items-center space-x-1">
+                <li className="flex items-center justify-center space-x-1">
                   <BsFillTelephoneFill />
                   <span>+123-456-7890</span>
                 </li>
-                <li className="flex items-center space-x-1">
+                <li className="flex items-center justify-center space-x-1">
                   <MdEmail />
                   <span className="">
                     <a
                       href="#"
-                      className="underline  hover:no-underline text-cyan-700"
+                      className="underline justify-center  hover:no-underline text-cyan-700"
                     >
                       support@technovasolutions.com
                     </a>
