64995ccc85505da970b7c0b2f1d637141e525a88
Izzsamara <izzidinsamara@gmail.com>
2025-04-12 14:12:04 +0300

    Update Margin, Padding, Text size, and Width on CTA component

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -3,14 +3,14 @@ export default function CTA() {
   return (
     <>
       <div className="mt-50 text-center w-full">
-        <h1 className="text-4xl  font-bold w-full">
+        <h1 className="text-xl md:text-4xl  font-bold w-full mx-auto">
           Ready to Transform Your Business with Cutting-Edge Tech Solutions?
         </h1>
-        <p className="text-2xl mt-8">
+        <p className="text-xs md:text-xl mt-8 p-1 ">
           Let’s discuss how TechNova can help you innovate and grow.
         </p>
         <Link to="/contact">
-          <button className="bg-cyan-700 font-bold hover:bg-cyan-800 p-4 w-3/5 md:w-1/5 mt-8  text-2xl text-white rounded-lg focus:ring-4 focus:ring-cyan-500 cursor-pointer">
+          <button className="bg-cyan-700 font-bold hover:bg-cyan-800 p-4 w-2/5 md:w-1/5 mt-8  text-md md:text-2xl text-white rounded-lg focus:ring-4 focus:ring-cyan-500 cursor-pointer">
             Contact Us now!
           </button>
         </Link>
