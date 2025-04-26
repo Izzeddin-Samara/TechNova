00d9f6797af0b6c07066c7d4c177fbfe8da0c362
Izzsamara <izzidinsamara@gmail.com>
2025-04-26 21:31:10 +0300

    Update Contact page layout and styles

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -23,28 +23,28 @@ const Contact: React.FC = () => {
         </div>
 
         <div className="w-full">
-          <div className="max-w-6xl bg-gray-100 mx-auto p-12 h-auto rounded-xl shadow-2xl bg-gray-200">
-            <div className="grid grid-cols-1 sm:grid-col-2 lg:grid-cols-2 gap-16">
+          <div className="max-w-7xl bg-gray-100 mx-auto h-auto ">
+            <div className="grid grid-cols-1 sm:grid-col-2 lg:grid-cols-2 gap-8">
               <div>
-                <form className="space-y-6" onSubmit={handleSubmit}>
+                <form className="space-y-6 p-4" onSubmit={handleSubmit}>
                   <div>
                     <label className="text-xl">Name</label>
                     <input
-                      className="w-full p-3 bg-gray-100 outline-none border border-gray-300 rounded-lg mt-2 focus:ring-3 focus:ring-cyan-600"
+                      className="w-full p-3 bg-gray-200 outline-none border border-gray-300 rounded-lg mt-2 focus:ring-3 focus:ring-cyan-600"
                       type="text"
                     />
                   </div>
                   <div>
                     <label className="text-xl">Email</label>
                     <input
-                      className="w-full p-3 bg-gray-100  border rounded-lg outline-none border border-gray-300 mt-2 focus:ring-3 focus:ring-cyan-600"
+                      className="w-full p-3 bg-gray-200  border rounded-lg outline-none border border-gray-300 mt-2 focus:ring-3 focus:ring-cyan-600"
                       type="text"
                     />
                   </div>
                   <div>
                     <label className="text-xl">Message</label>
                     <textarea
-                      className="w-full h-48 p-3 bg-gray-100 border outline-none rounded-lg border-gray-300 mt-2 resize-none focus:ring-3 focus:ring-cyan-600"
+                      className="w-full h-48 p-3 bg-gray-200 border outline-none rounded-lg border-gray-300 mt-2 resize-none focus:ring-3 focus:ring-cyan-600"
                       name="Message"
                       id=""
                     ></textarea>
@@ -88,7 +88,7 @@ const Contact: React.FC = () => {
                     Call us to speak to a member of our team. We are
                     <span className="block">always happy to help.</span>
                   </p>
-                  <p className="text-cyan-700 font-bold">+123-456-7890</p>
+                  <p className="text-cyan-700 font-bold mt-4">+123-456-7890</p>
                 </div>
               </div>
             </div>
