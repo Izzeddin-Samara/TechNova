4703099dce6b0476528565a1b0ed0e70e2057460
Izzsamara <izzidinsamara@gmail.com>
2025-04-28 15:15:38 +0300

    Update button width in Services component

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -124,7 +124,7 @@ export default function Services() {
           drive success.{" "}
         </p>
         <Link to="/contact">
-          <button className="bg-cyan-700 font-bold hover:bg-cyan-800 p-4 md:w-2/5 mt-8  text-md md:text-xl text-white rounded-lg focus:ring-4 focus:ring-cyan-500 cursor-pointer">
+          <button className="bg-cyan-700 font-bold hover:bg-cyan-800 p-4 md:w-1/5 mt-8  text-md md:text-xl text-white rounded-lg focus:ring-4 focus:ring-cyan-500 cursor-pointer">
             Request a Free Consultation
           </button>
         </Link>
