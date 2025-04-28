d1487a952b34ab1db4785e82ffb1a9108a7a253a
Izzsamara <izzidinsamara@gmail.com>
2025-04-29 00:44:08 +0300

    Update styles for Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,14 +39,14 @@ export default function Footer() {
   ];
   return (
     <>
-      <footer className="mt-50 bg-gray-200 h-auto p-8 pb-1">
-        <div className="max-w-8xl mx-auto text-center">
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 space-y-8 space-x-4">
+      <footer className="mt-50 bg-gray-200 h-auto p-4 pt-8 pb-1">
+        <div className="max-w-[95%] mx-auto text-center">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 space-y-8 space-x-2">
             <div>
-              <h1 className="text-3xl font-bold">
+              <h1 className="text-3xl font-bold text-left">
                 Tech<span className="text-cyan-700">Nova</span>
               </h1>
-              <p className="w-3/4 text-sm text-justify mt-5 mb-4 mx-auto">
+              <p className=" text-sm text-justify mt-5 mb-4 mx-auto">
                 At Technova, we are committed to empowering your business by
                 delivering cutting-edge, future-ready technology solutions that
                 are secure, scalable, and tailored to meet your unique goals.
@@ -56,11 +56,11 @@ export default function Footer() {
             </div>
             <div>
               <h1 className="font-bold text-xl">Company</h1>
-              <ul className="space-y-4 mt-6">
+              <ul className="space-y-6 mt-6">
                 <li>
                   <Link
                     to="/about"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     About us
                   </Link>
@@ -68,7 +68,7 @@ export default function Footer() {
                 <li>
                   <Link
                     to="/services"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     Our Services
                   </Link>
@@ -76,7 +76,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="#"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     Pricing
                   </a>
@@ -84,7 +84,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="#"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     Blog
                   </a>
@@ -92,7 +92,7 @@ export default function Footer() {
                 <li>
                   <Link
                     to="/careers"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     {" "}
                     Careers
@@ -102,11 +102,11 @@ export default function Footer() {
             </div>
             <div>
               <h1 className="font-bold text-xl">Support</h1>
-              <ul className="space-y-4 mt-6 mb-6 ">
+              <ul className="space-y-6 mt-6 mb-6 ">
                 <li>
                   <a
                     href="#"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     FAQ
                   </a>
@@ -114,7 +114,7 @@ export default function Footer() {
                 <li>
                   <Link
                     to="/contact"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     Contact Us
                   </Link>
@@ -122,7 +122,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="#"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     Help Center
                   </a>
@@ -130,7 +130,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="#"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     Terms and conditions
                   </a>
@@ -138,7 +138,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="#"
-                    className="hover:text-cyan-700 text-lg hover:underline"
+                    className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                   >
                     Privacy Policy
                   </a>
@@ -147,12 +147,12 @@ export default function Footer() {
             </div>
             <div>
               <h1 className="font-bold text-xl">Solutions</h1>
-              <ul className="space-y-4 mt-6">
+              <ul className="space-y-6 mt-6">
                 {ServicesLinks.map((link, index) => (
                   <li key={index}>
                     <Link
                       to={`/services#${link.slug}`}
-                      className="hover:text-cyan-700 text-lg hover:underline"
+                      className="border-b-2 border-transparent hover:border-cyan-700 pb-1 transition-all text-sm"
                     >
                       {link.title}
                     </Link>
@@ -162,21 +162,21 @@ export default function Footer() {
             </div>
             <div>
               <h1 className="font-bold text-xl ">Connect with Us</h1>
-              <ul className="space-y-4 mt-6">
+              <ul className="space-y-8 mt-7">
                 <li className="flex items-center justify-center space-x-1">
-                  <FaLocationDot />
-                  <span>123 TechNova Street, City, Country</span>
+                  <FaLocationDot size={20} />
+                  <span className="text-sm">123 TechNova Street, City, Country</span>
                 </li>
                 <li className="flex items-center justify-center space-x-1">
-                  <BsFillTelephoneFill />
-                  <span>+123-456-7890</span>
+                  <BsFillTelephoneFill size={20} />
+                  <span className="text-sm">+123-456-7890</span>
                 </li>
                 <li className="flex items-center justify-center space-x-1">
-                  <MdEmail />
-                  <span className="">
+                  <MdEmail size={20} className="mt-1" />
+                  <span>
                     <a
                       href="#"
-                      className="underline justify-center  hover:no-underline text-cyan-700"
+                      className="underline justify-center text-sm  hover:no-underline text-cyan-700"
                     >
                       support@technovasolutions.com
                     </a>
@@ -191,7 +191,7 @@ export default function Footer() {
                     rel="noopener noreferrer"
                     className="hover:text-gray-500"
                   >
-                    <FaGithub size={30} />
+                    <FaGithub size={20} />
                   </a>
                 </li>
                 <li>
@@ -201,7 +201,7 @@ export default function Footer() {
                     rel="noopener noreferrer"
                     className="hover:text-gray-500"
                   >
-                    <FaLinkedin size={30} />
+                    <FaLinkedin size={20} />
                   </a>
                 </li>
                 <li>
@@ -211,7 +211,7 @@ export default function Footer() {
                     rel="noopener noreferrer"
                     className="hover:text-gray-500"
                   >
-                    <FaFacebookSquare size={30} />
+                    <FaFacebookSquare size={20} />
                   </a>
                 </li>
                 <li>
@@ -221,11 +221,11 @@ export default function Footer() {
                     rel="noopener noreferrer"
                     className="hover:text-gray-500"
                   >
-                    <AiFillInstagram size={30} />
+                    <AiFillInstagram size={20} />
                   </a>
                 </li>
               </ul>
-              <p className="mt-10">
+              <p className="mt-10 text-sm">
                 Created by{" "}
                 <a
                   className="hover:underline text-cyan-700"
